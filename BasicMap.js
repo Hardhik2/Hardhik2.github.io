85535561146adb6fcc6cbffd352059340af2c90e
Hardhik2 <hardhik23.mandava@gmail.com>
2024-07-18 15:35:52 -0400

    Update BasicMap.js

--- BasicMap.js
+++ BasicMap.js
@@ -1,14 +1,11 @@
 console.log("BasicMap.js loaded");
 // CPCC Coordinates: 35.2176665 N,80.831473 W ->35.2176665,-80.831473
-let map= L.map("map",{
-  center:[35.2176665,-80.831473],
-  zoom:16
-});
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
+
+let markers=[];
+
+markers.push(
 L.marker([35.2176665,-80.831473],{
   title:"Hello From CPCC!!"
 }).bindPopup(`
@@ -21,4 +18,31 @@ L.marker([35.2176665,-80.831473],{
 <p>[URL:https://www.youtube.com/@centralpiedmont<br>communityco39/videos]</p>
 </center>
 `)
-  .addTo(map);
+)
+let CPCC= L.layerGroup(markers);
+
+let street=L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+var topo = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+	maxZoom: 20,
+	attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+});
+let baseMaps={
+  Street: street,
+  Topograpic: topo 
+}
+
+let overlayMaps={
+  CPCC:CPCC
+}
+let map= L.map("map",{
+  center:[35.2176665,-80.831473],
+  zoom:16,
+  layers:[street,CPCC]
+});
+
+L.control.layers(baseMaps,overlayMaps,{
+  collapsed:false
+}).addTo(map);
